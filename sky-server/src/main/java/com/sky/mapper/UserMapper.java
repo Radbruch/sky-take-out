1dca77abf61a9c8b8954965d770da4092f6bb9a1
Radbruch <228806897@qq.com>
2024-09-17 02:09:27 +0800

    微信user登录

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.sky.mapper;
+
+import com.sky.annocation.AutoFill;
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+    @Select("SELECT * FROM user WHERE openid = #{openid}")
+    User getUserByOpenId(String openid);
+
+
+    void insert(User user);
+}
