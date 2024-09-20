3bef6ede6916d9e13d4ef0fff31dea9f6e7d4be4
Radbruch <228806897@qq.com>
2024-09-20 11:48:18 +0800

    Order和Payment相关常量

---
+++ sky-common/src/main/java/com/sky/constant/OrderConstant.java
@@ -0,0 +1,19 @@
+package com.sky.constant;
+
+/**
+ * @ClassName OrderConstant
+ * @Description TODO
+ * @Author msjoy
+ * @Date 2024/9/19 20:06
+ * @Version 1.0
+ **/
+public class OrderConstant {
+
+    public static final Integer Pending_Payment= 1;
+    public static final Integer PENDING_ACCEPTED = 2;
+    public static final Integer ORDER_ACCEPTED= 3;
+    public static final Integer OUT_FOR_DELIVERY= 4;
+    public static final Integer COMPLETED= 5;
+    public static final Integer CANCELLED= 6;
+    public static final Integer REFUNDED= 17;
+}
---
+++ sky-common/src/main/java/com/sky/constant/PaymentConstant.java
@@ -0,0 +1,15 @@
+package com.sky.constant;
+
+/**
+ * @ClassName PaymentConstant
+ * @Description TODO
+ * @Author msjoy
+ * @Date 2024/9/19 20:22
+ * @Version 1.0
+ **/
+public class PaymentConstant {
+
+    public static final Integer NOT_PAID= 0;
+    public static final Integer PAID= 1;
+    public static final Integer REFUND = 2;
+}
